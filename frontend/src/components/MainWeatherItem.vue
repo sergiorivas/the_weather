775b30ad842cb08e2c4349becefe04a05d0c35d0
Sergio Rivas <sergiorivas@gmail.com>
2023-03-01 22:39:51 -0500

    feat: adding main component

--- frontend/src/components/MainWeatherItem.vue
+++ frontend/src/components/MainWeatherItem.vue
@@ -7,10 +7,30 @@ export default {
 };
 </script>
 <template>
-	<div>
-		ADDRESS1
-		{{ address }}
-		CARD
-		{{ weatherInfo }}
+	<div class="bg-white text-blue-800 px-3 py-6 rounded-lg mt-3 mx-auto w-7/12">
+		<div class="text-center text-orange-800 text-xl font-bold">
+			{{ address.formalAddress }}
+		</div>
+		<div class="text-center font-bold">{{ address.latitude }}, {{ address.longitude }}</div>
+		<div class="text-center text-l">WIND: {{ weatherInfo.windSpeed }} ({{ weatherInfo.windDirection }})</div>
+		<div class="bg-white text-blue-800 px-3 py-6 rounded-lg mt-3 mx-auto flex justify-between">
+			<div class="text-left w-1/3">
+				<div class="text-xl">
+					{{ weatherInfo.name }}
+				</div>
+				<div class="text-4xl text-orange-700">{{ weatherInfo.temperature }} {{ weatherInfo.temperatureUnit }}&deg;</div>
+			</div>
+			<div>
+				<img class="rounded-full" :src="weatherInfo.iconUrl" />
+			</div>
+			<div class="w-1/3 text-xl">
+				<div class="text-end text-black">
+					<div>{{ weatherInfo.summary }}</div>
+				</div>
+			</div>
+		</div>
+		<div class="text-center font-bold">
+			{{ weatherInfo.forecastDescription }}
+		</div>
 	</div>
 </template>
